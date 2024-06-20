7f2a61195575a82d1cfb5c83c610882dd51ff5d8
pius <tebulapius@gmail.com>
2024-06-20 11:00:22 +0300

    Myassets change
    Myassets to assets

--- yii2-basic/views/myassets/create.php
+++ yii2-basic/views/myassets/create.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\Myassets $model */
 
-$this->title = 'Create Myassets';
+$this->title = 'Create Asset';
 $this->params['breadcrumbs'][] = ['label' => 'Myassets', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
--- yii2-basic/views/myassets/index.php
+++ yii2-basic/views/myassets/index.php
@@ -10,7 +10,7 @@ use yii\grid\GridView;
 /** @var app\models\MyassetsSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Myassets';
+$this->title = 'Assets';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="myassets-index">
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Myassets', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Asset', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
